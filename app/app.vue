4fac7a19cd8f3a567fb8adb252faacfbcc1d356a
Apil Khadka <apilkhadka2020@gmail.com>
2025-09-11 09:39:45 +0545

    eslint  tailwind adding now

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold text-blue-500 underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-secondary justify-center">
+      Mapping Map <Icon name="tabler:map" size="24" />
+    </button>
   </div>
 </template>
