7915bc51116eeae762b802a1f85b2d3f02b9bc4d
Apil <apilkhadka2020@gmail.com>
2025-09-11 09:48:38 +0545

    setup tailwind/css /daisyui/icons (#48)
    * eslint  tailwind adding now
    
    * done

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class=" font-bold text-blue-500 text-5xl underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-secondary justify-center">
+      Mapping Map <Icon name="tabler:map" size="24" />
+    </button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
+import tailwindcss from "@tailwindcss/vite";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
   eslint: {
-
     config: {
       standalone: false,
     },
+  },
+  css: [resolve(__dirname, "assets/css/app.css")],
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
