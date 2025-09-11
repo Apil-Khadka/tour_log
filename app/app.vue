4e4c6ddd91d5ebb15bf4629e197a5846615bdec4
Apil <apilkhadka2020@gmail.com>
2025-09-11 11:24:55 +0545

    setup project, under app folder (#49)

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class=" font-bold text-blue-500 text-5xl underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-secondary justify-center">
-      Mapping Map <Icon name="tabler:map" size="24" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="text-blue-700 underline">
+    Hello World
+  </h1>
+</template>
