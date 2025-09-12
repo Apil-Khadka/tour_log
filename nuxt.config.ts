62e25f916f9d59414e9f7d96861ee1b3deab58d9
Apil <apilkhadka2020@gmail.com>
2025-09-12 10:52:11 +0545

    zod setup (#51)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // nuxt.config.ts
 import tailwindcss from "@tailwindcss/vite";
 
+import "./lib/env";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
