3b2f424f5f0262ae0b398c10f8abf907b32f0f65
Apil Khadka <apilkhadka2020@gmail.com>
2025-09-13 11:56:39 +0545

    :fire: Done google sign in

---
+++ app/pages/error.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const route = useRoute();
+const error = route.query.error || "An unknown error occurred";
+</script>
+
+<template>
+  <div class="card bg-base-400 container mt-5 min-h-72 text-center mx-auto flex flex-col justify-center items-center gap-4">
+    <div role="alert" class="alert alert-error min-w-1/2  text-center">
+      <span>Error! {{ error }}</span>
+    </div>
+    <NuxtLink to="/" class="btn btn-primary">
+      Home <Icon name="tabler:arrow-left" size="33" />
+    </NuxtLink>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import AuthButton from "~/components/auth-button.vue";
+</script>
+
 <template>
   <div class="hero bg-base-300 container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
@@ -8,10 +12,7 @@
         <p class="py-6">
           Keep track of your travels and adventures with this Travel Log.
         </p>
-        <button class="btn btn-accent">
-          Sign In With GitHub
-          <Icon name="tabler:brand-github" size="24" />
-        </button>
+        <AuthButton />
       </div>
     </div>
   </div>
